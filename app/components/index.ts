95d0d90eb8f7f210ab3936b8e9589e2f939ef469
Fermanuel <manuelhola9@gmail.com>
2024-03-22 12:46:41 -0700

    agregue un chart al dasboard

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,3 +2,4 @@ export { CardHero } from './Card/Card';
 export { Login } from './Login/Login';
 export { Sidebar } from './Sidebar/Sidebar';
 export { SidebarItem } from './SidebarItem/SidebarItem';
+export { AreaChartHero } from './ChartArea/ChartArea';
--- app/dashboard/panel/page.tsx
+++ app/dashboard/panel/page.tsx
@@ -1,11 +1,55 @@
+"use client";
 
+import { Card } from "@tremor/react";
+import { AreaChartHero } from "@/app/components";
 
 export default function PanelPage() {
     return (
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <div className="grid grid-cols-4 grid-rows-5 gap-4 pt-5">
-
+      <div className="flex flex-col items-center justify-center flex-1 px-5">
+        <div className="grid grid-cols-4 gap-3 pt-3 w-full">
+          <Card>
+            <div>
+                <h1 className="text-xl text-white">Citas Realizadas</h1>
+            </div>
+            <div className="pt-5 text-right">
+                <span className="text-white text-xl">10</span>
+            </div>
+          </Card>
+          <Card>
+            <div>
+                <h1 className="text-xl text-white">Citas Canceladas</h1>
+            </div>
+            <div className="pt-5 text-right">
+                <span className="text-white text-xl">2</span>
+            </div>
+          </Card>
+          <Card>
+            <div>
+                <h1 className="text-xl text-white">Pacientes Atendidos</h1>
+            </div>
+            <div className="pt-5 text-right">
+                <span className="text-white text-xl">6</span>
+            </div>
+          </Card>
+          <Card>
+            <div>
+                <h1 className="text-xl text-white">No. Pacientes</h1>
+            </div>
+            <div className="pt-5 text-right">
+                <span className="text-white text-xl">45</span>
+            </div>
+          </Card>
         </div>
-      </main>
+
+        <div className="grid grid-cols-4 grid-rows-5 gap-4 pt-3 w-full">
+            <span className="text-xl text-gray-700">Estadisticas del mes</span>
+
+            <div className="pt-3">
+                <Card>
+                    <AreaChartHero />
+                </Card>
+            </div>
+          </div>
+      </div>
     );
 }

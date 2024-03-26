ef2a24132bda12ae785b1c9c29cf8118783d353b
Fernando <manuelhola9@gmail.com>
2024-03-25 23:54:29 -0700

    react big calendar agregado correctamente

--- app/dashboard/citas/page.tsx
+++ app/dashboard/citas/page.tsx
@@ -7,7 +7,7 @@ export default function citasPage(){
   return (
     <div className="grid grid-cols-5 grid-rows-5 gap-4 p-10">
       <div className="col-span-5 row-span-5 flex items-center justify-center">
-        <Card className="w-full h-full">
+        <Card className="w-screen h-screen">
           <Calendario/>
         </Card>
       </div>

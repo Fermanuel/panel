ac61309570b514f590184ad38e74528fd7ec01a6
Fernando <manuelhola9@gmail.com>
2024-03-28 20:45:22 -0700

    ajustes de tamaño del calendario v2

--- app/dashboard/citas/page.tsx
+++ app/dashboard/citas/page.tsx
@@ -7,12 +7,12 @@ import { Button } from "@tremor/react";
 export default function citasPage(){  
   return (
     <div className="container mx-auto px-5">
-      <div className="col-span-4 row-span-5 flex items-center justify-center">
+      <div className="grid-cols-4 gap-3 pt-3 flex items-center justify-center">
         <Card className="w-screen h-screen">
           <Calendario/>
         </Card>
       </div>
-      <div className="col-span-4 row-span-1 flex items-end justify-end pt-3">
+      <div className="col-span-4 row-span-1 flex items-end justify-end pt-5">
         <Button>Crear nueva cita</Button>
       </div>
     </div>
